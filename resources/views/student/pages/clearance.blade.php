0ac23106ea11e04dd83cbe412831c656e2459b2f
MaureenDadap <maureendadap@gmail.com>
2022-06-13 09:08:23 +0800

    created template for clearance page

--- resources/views/student/pages/clearance.blade.php
+++ resources/views/student/pages/clearance.blade.php
@@ -1,3 +1,28 @@
 @extends('student.layouts.default')
 @section('title', '- Clearance')
 @section('body-title', 'Clearance')
+@section('content')
+
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <h5><i class="bi-check-circle"></i> Student's Accountabilities</h5>
+            <hr>
+            <div class="row align-items-center gy-4">
+                <div class="col-md-3">
+                    <img class="student-pic" src="../img/user.png" alt="user image">
+                </div>
+                <div class="col-md-9">
+                    <p><strong>Student ID:</strong> Lorem ipsum</p>
+                    <p><strong>Student Name:</strong> Lorem ipsum</p>
+                    <p><strong>Student Program:</strong> Lorem ipsum</p>
+                </div>
+            </div>
+            <hr>
+            <div class="alert alert-success">
+                No pending accountabilities.
+            </div>
+        </div>
+    </div>
+
+
+@endsection
